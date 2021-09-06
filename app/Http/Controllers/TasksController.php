be7bb21620d792869ce9e983b93aaf6989d93275
hana.hirano <h.87_rad@icloud.com>
2021-09-06 11:05:02 +0000

    kadai_3

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -78,6 +78,9 @@ class TasksController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $task = Task::findOrFail($id);
+        $task->delete();
+        
+        return redirect('/');
     }
 }
